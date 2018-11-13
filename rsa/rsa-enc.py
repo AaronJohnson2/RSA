ec75d60ca8de5f23cac0567d8e8657d80703345b
hcoffey1 <hcoffey1@vols.utk.edu>
2018-11-12 23:55:31 -0500

    Added RSA encyption

--- rsa/rsa-enc.py
+++ rsa/rsa-enc.py
@@ -2,12 +2,14 @@
 #sys.path.append("lib")
 import halib2
 import math
+import os
 
 if __name__ == "__main__":
     args = halib2.arg_return(0)    
 
     public_file = open(args.k, "r")    
     message_file = open(args.i, "r")
+    cipher_file = open(args.o, 'w')
 
     size_N = int(public_file.readline())
     N = int(public_file.readline())
@@ -17,7 +19,28 @@ if __name__ == "__main__":
     tmpM = message_file.read().replace('\n','')
     hexM = bytes.fromhex(tmpM)
     m = int.from_bytes(hexM, byteorder='big')
-    print(m)
     #size_m = math.floor(math.log(m,2))+1
+#    print(size_N)
+
     size_m = len(hexM)*8
-    print(size_m)
+
+#    print(size_m)
+
+    #if size_m > (size_N - 2):
+    if size_m > (size_N - 8):
+        print("Invalid message size")
+        exit(0)
+
+    #print(os.urandom((size_m)/8 - 2))
+    r = os.urandom(int((size_m)/8 - 1))
+#    print(r)
+#    print(hexM)
+
+    m_hat = int.from_bytes(r + hexM, byteorder='big')
+    c = pow(m_hat,e,N)
+
+    cipher_file.write(str(c))
+
+    message_file.close()
+    public_file.close()
+    cipher_file.close()
